a198c8e9e091c2dfa0b5778533225021d46d4273
Sourabh Shukla <sourabhshukla3.14@gmail.com>
2022-03-27 18:05:49 +0530

    project hovering info

--- src/components/Projects/Project/Project.js
+++ src/components/Projects/Project/Project.js
@@ -1,12 +1,20 @@
 import React,{useEffect,useState} from 'react';
 import "./Project.css"
 
+import {FaPlay, FaCode} from "react-icons/fa"
 const Project = ({data}) => {
 
     return(
         <div className='project' >
             <h2>{data.name}</h2>
             <img src={data.img} />
+            <div className='info'>
+                <p>{data.desc}</p>
+                <div id="btn-cont">
+                    <a className="link-btn" href={data.code_link}><FaCode /></a>
+                    <a className="link-btn" href={data.live_link}><FaPlay /></a>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -14,7 +14,7 @@ const projectData = [
         name: "YelpCamp",
         code_link: "https://github.com/anonymous372/YelpCamp",
         live_link: "https://yelpcamp-anonymous372.herokuapp.com/",
-        desc: "This is a WebDev Project",
+        desc: "This is a community site for CAMPERS. You can create your account, create or update your camprounds. Read about others camprounds and comment on them",
         img: img1,
         vid: "/"
     },
@@ -22,7 +22,7 @@ const projectData = [
         name: "Blog App",
         code_link: "https://github.com/anonymous372/Blog-App",
         live_link: "https://blogapp-anonymous372.herokuapp.com/",
-        desc: "This is a WebDev Project",
+        desc: "This is a Blog App where you can Create, Read, Update and Delete a blog. Beautiful and Responsive Web Design",
         img: img2,
         vid: "/"
     },
@@ -30,7 +30,7 @@ const projectData = [
         name: "Holdlinfo Clone",
         code_link: "https://github.com/anonymous372/Holdlinfo",
         live_link: "https://holdlinfo-clone.herokuapp.com/",
-        desc: "This is a WebDev Project",
+        desc: "This is a clone of site Holdlinfo.com. Here you can get current value of some of the famous cryptocurrencies. I have used the WazirX APi to fetch data",
         img: img3,
         vid: "/"
     },
@@ -38,7 +38,7 @@ const projectData = [
         name: "Sorting Visualizer",
         code_link: "https://github.com/anonymous372/Sorting-Visualizer",
         live_link: "https://anonymous372.github.io/Sorting-Visualizer/",
-        desc: "This is a WebDev Project",
+        desc: "This is a Simple Project where you can see sorting happen right before your eyes. Choose between some of the famous sorting algorithms, change the speed or number of elements & Play with it!",
         img: img4,
         vid: "/"
     },
@@ -46,7 +46,7 @@ const projectData = [
         name: "Sample Website",
         code_link: "https://github.com/anonymous372/Sample-Website",
         live_link: "https://anonymous372.github.io/Sample-Website/",
-        desc: "This is a WebDev Project",
+        desc: "This is a Sample Website created using just the HTML,CSS and JS",
         img: img5,
         vid: "/"
     }
