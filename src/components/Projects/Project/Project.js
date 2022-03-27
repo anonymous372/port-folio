fda4a629ff8af055825627722195ed94b1eb6234
Sourabh Shukla <sourabhshukla3.14@gmail.com>
2022-03-27 16:48:40 +0530

    Rollback

--- src/components/Projects/Project/Project.js
+++ src/components/Projects/Project/Project.js
@@ -1,15 +1,11 @@
 import React,{useEffect,useState} from 'react';
 import "./Project.css"
 
-const onProject = (event) => {
-    // event.style.color = "red"
-    console.log("Mouse over ",event)
-}
 
 const Project = ({data}) => {
 
     return(
-        <div className='project' onMouseOver={onProject}>
+        <div className='project' >
             <h2>{data.name}</h2>
             <img src={data.img} />
         </div>
