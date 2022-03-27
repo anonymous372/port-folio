3fc65c34f6798809454579df7909898a140daf1e
Sourabh Shukla <sourabhshukla3.14@gmail.com>
2022-03-27 17:11:14 +0530

    fixed express icon

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -3,13 +3,16 @@ import "./Skills.css"
 
 
 import {FaNodeJs, FaDatabase, FaReact, FaCss3} from "react-icons/fa"
+import exIcon from "../../images/expressjs.svg"
+
+
 const Skills = () => {
     return(
         <div id='skills'>
             <h1>Skills</h1>
             <div id="skill-list">
                 <div className="skill"><FaNodeJs className='icon'></FaNodeJs> Node</div>                
-                <div className="skill"><img src="./expressjs1.svg" className="icon" height="18px" width="18px"></img> Express</div>                
+                <div className="skill"><img src={exIcon} className="icon" height="18px" width="18px"></img> Express</div>                
                 <div className="skill"><FaDatabase className='icon'></FaDatabase> MongoDB</div>                
                 <div className="skill"><FaReact className='icon'></FaReact> ReactJS</div>                
                 <div className="skill"><FaCss3 className='icon'></FaCss3> CSS</div>                
